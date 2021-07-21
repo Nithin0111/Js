4051570a1ac4585579a3a2b566b1c257f6344c8e
nithin <programming965@gmail.com>
2021-07-21 20:40:34 +0530

    day 4 files added

---
+++ day4.js
@@ -0,0 +1,30 @@
+// day4 - 21/07/21
+
+// lesson 34 - declaring multiple values for variable
+
+// variable a should have values of nithin,suresh and pandu
+
+// array is a collection of value value might be of different data types
+
+// most important datastructure
+// arrays are mutable
+
+var a = ["nithin", "pandu", "suresh"];
+console.log(a);
+
+//output only nithin
+
+//lesson 35 - nesting arrays
+
+// ["nithin","pandu"] => "nithin" ["nithin","suresh"] => pandu in variable b
+var b = [
+  ["nithin", "pandu"],
+  ["nithin", "suresh"],
+];
+console.log(b);
+
+//output resh"];36 - changing values in array since arrays are mutable
+
+var a = ["nithin", "pandu", "suresh"];
+a[0] = "kittu";
+console.log(a);
